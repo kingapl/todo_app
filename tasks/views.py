c5280cd365683dfc094b362ab7815f1ccfa5e551
kingapl <kingapluciennik@wp.pl>
2021-02-26 13:45:19 +0100

    Save tasks to the database.

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import TaskForm
 
 
 def index(request):
-    form = TaskForm()
+    if request.method != 'POST':
+        form = TaskForm()
+    else:
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:index')
 
     context = {'form': form}
     return render(request, 'tasks/index.html', context)
